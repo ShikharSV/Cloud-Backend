24d5f8a0671661ed21002b1405a30440f66b4ee1
ShikharSV <shikhar7409@gmail.com>
2023-01-15 14:06:16 +0530

    created login endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -49,4 +49,38 @@ router.post('/createuser',[
         res.send(500).send("Some error occured");
     }
 })
+
+// Authenticate a user using: POST "api/auth/login"  No login required
+router.post('/login',[
+    body('email','Enter a valid email').isEmail(),
+    body('password','Cannot be blank').exists(),
+],async(req, res)=>{
+    // If errors, return bad request and errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password}=req.body;
+    try {
+        let user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Invalid Credentials-2"});
+        }
+        const passwordCompare=await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Invalid Credentials"});
+        }
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const authtoken=jwt.sign(data,JWT_SECRET);
+        res.send({authtoken});
+    } catch (error) {
+        console.error(error.message);
+        res.send(500).send("Internal Server Error");
+    }
+    
+})
 module.exports=router;  
