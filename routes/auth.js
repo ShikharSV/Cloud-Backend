9d20e4df43504d4eeaa90831a18fcbf3ff671962
ShikharSV <shikhar7409@gmail.com>
2023-01-15 11:44:27 +0530

    Used express validator for validating request, made thumder client collections

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,37 @@
+const { application } = require('express');
 const express=require('express');
 const router=express.Router();
+const User=require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req, res)=>{
-    res.json([]);
+
+// Create a user using: POST "api/auth/createuser"  No login required
+router.post('/createuser',[
+    body('password','Enter a valid name').isLength({ min: 3 }),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Must must be atleast 8 characters').isLength({ min: 8 })
+],async(req, res)=>{
+    // If errors, return bad request and errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } 
+    // Check whether a user with this email exits already  
+    try {
+        let user=await User.findOne({email:req.body.email});
+    if(user){
+        return res.status(400).json({error: "Email already exits"})
+    }
+    user= await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+    res.json(user)
+    } catch (error) {
+        console.error(error.message);
+        res.send(500).send("Some error occured");
+    }
 })
 
-module.exports=router;
+module.exports=router;  
